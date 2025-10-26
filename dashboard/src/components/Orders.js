cf1fa198c68e5558427ff38bb1d82aac36326bc6
unknown <amanbansal9045069517@gmail.com>
2025-10-26 11:08:24 +0530

    Re-add frontend and dashboard as regular folders

---
+++ dashboard/src/components/Orders.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from 'axios'
+
+const Orders = () => {
+//   return (
+//     <div className="orders">
+//       <div className="no-orders">
+//         <p>You haven't placed any orders today</p>
+
+//         <Link to={"/"} className="btn">
+//           Get started
+//         </Link>
+//       </div>
+//     </div>
+//   );
+
+  const [allPositions,setAllPositions]=useState([]);
+    useEffect(()=>{
+      axios.get("http://localhost:3002/allOrders").then((res)=>{
+        setAllPositions(res.data)
+      });
+    },[])
+
+  return (
+    <>
+      <h3 className="title">Positions ({allPositions.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+           
+            <th>Instrument</th>
+            <th>Qty.</th>
+            <th>Avg.</th>
+            <th>LTP</th>
+            <th>P&L</th>
+            <th>Chg.</th>
+          </tr>
+
+          {allPositions.map((stock, index) => {
+            const curValue = stock.price * stock.qty;
+            const isProfit = curValue - stock.avg * stock.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";
+            const dayClass = stock.isLoss ? "loss" : "profit";
+
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                 <td>{stock.price}</td>
+
+                {/* <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td> */}
+                <td className={profClass}>
+                  {(curValue - stock.avg * stock.qty).toFixed(2)}
+                </td>
+                <td className={dayClass}>{stock.day}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </>
+  );
+
+};
+
+export default Orders;

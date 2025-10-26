fdbf09cbf4b39ea32d251ef582e015eeb863c203
Amankumar-debug <amanbansal9045069517@gmail.com>
2025-10-26 23:02:40 +0530

    Update Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -90,7 +90,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://zerodha-clone-d8et.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
